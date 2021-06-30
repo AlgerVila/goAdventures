7af52d7b3cb0c07824174331bff5225d3a3315cc
carla-vila <algervila>
2021-07-01 01:03:41 +0200

    hello

--- FromGoToCsv.go
+++ FromGoToCsv.go
@@ -1,1 +1,30 @@
+package main
 
+import (
+	"encoding/csv"
+	"log"
+	"os"
+)
+
+func main() {
+	rows := [][]string{
+		{"Country", "city", "state"},
+		{"usa", "SanJose", "California"},
+		{"usa", "dallas", "texas"},
+	}
+
+	csvfile, err := os.Create("data.csv")
+
+	if err != nil {
+		log.Fatalf("Failed to create file")
+	}
+
+	cswriter := csv.NewWriter(csvfile)
+
+	for _, row := range rows {
+		_ = cswriter.Write(row)
+	}
+	cswriter.Flush()
+	csvfile.Close()
+
+}
